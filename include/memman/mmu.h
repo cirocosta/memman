f72ed59734db1552b2f235ad188cdc4a2d43bf98
Ciro S. Costa <ciro9758@gmail.com>
2015-10-16 07:39:57 -0300

    ep2

--- include/memman/mmu.h
+++ include/memman/mmu.h
@@ -29,7 +29,7 @@ typedef struct mm_mmu_t {
   uint8_t frame_size_bits;
   uint8_t offset_mask;
 
-  unsigned char* free_pageframes; // 0 ==> not free. 1 ==> fre
+  unsigned char* free_pageframes; // 0 ==> not free. 1 ==> free
   unsigned free_pageframes_count;
 } mm_mmu_t;
 
